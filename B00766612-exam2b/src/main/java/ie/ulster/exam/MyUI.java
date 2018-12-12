05cd5773206db8a598a07e1729900b8654db94d1
Gillian <Macnicholas-G@ulsteruniversity.com>
2018-12-12 17:16:10 +0000

    final

--- B00766612-exam2b/src/main/java/ie/ulster/exam/MyUI.java
+++ B00766612-exam2b/src/main/java/ie/ulster/exam/MyUI.java
@@ -46,131 +46,128 @@ public class MyUI extends UI {
 
 // Create the connection object
 Connection connection = null;  
+
 try 
 {
 	// Connect with JDBC driver to a database
     connection = DriverManager.getConnection(connectionString);
     // Add a label to the web app with the message and name of the database we connected to 
-    layout.addComponent(new Label("Connected to database: " + connection.getCatalog()));
+    layout.addComponent(new Label("Connected to database: " + connection.getCatalog())); //remove when working
 	 
-ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM BusDetails;");
-// Convert the resultset that comes back into a List 
-List<Bus> buses = new ArrayList<Bus>();
-// While there are more records in the resultset
-while(rs.next())
-{   
-	// Add a new Customer instantiated with the fields from the record (that we want, we might not want all the fields, note how I skip the id)
-	buses.add(new Bus(rs.getString("destination"), 
+    ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM BusDetails;");
+    // Convert the resultset that comes back into a List 
+    List<Bus> buses = new ArrayList<Bus>();
+    // While there are more records in the resultset
+    while(rs.next())
+        {   
+	    // Add a new Customer instantiated with the fields from the record (that we want, we might not want all the fields, note how I skip the id)
+	    buses.add(new Bus(rs.getString("destination"), 
 				rs.getInt("maxPeople"), 
 				rs.getString("feature"), 
                 rs.getBoolean("accessible")));
 
-} 
-// Add my component, grid is templated with Customer
-Grid<Bus> myGrid = new Grid<>();
-// Set the items (List)
-myGrid.setItems(buses);
-// Configure the order and the caption of the grid
-myGrid.addColumn(Bus::getDestination).setCaption("Destination");
-myGrid.addColumn(Bus::getPeople).setCaption("MaxPeople");
-myGrid.addColumn(Bus::getFeature).setCaption("Feature");
-myGrid.addColumn(Bus::isAccessible).setCaption("Accessible");
+        } 
+    // Add my component, grid is templated with Customer
+    Grid<Bus> myGrid = new Grid<>();
+    // Set the items (List)
+    myGrid.setItems(buses);
+    // Configure the order and the caption of the grid
+    myGrid.addColumn(Bus::getDestination).setCaption("Destination");
+    myGrid.addColumn(Bus::getPeople).setCaption("MaxPeople");
+    myGrid.addColumn(Bus::getFeature).setCaption("Feature");
+    myGrid.addColumn(Bus::isAccessible).setCaption("Accessible");
 
-myGrid.setSizeFull(); // This makes the grid the width of the page
-        // This makes the grid 'multi-select', adds the checkboxes for selecting to the side
-        myGrid.setSelectionMode(SelectionMode.MULTI);
-        MultiSelect <Bus> selected = myGrid.asMultiSelect();
+    myGrid.setSizeFull(); // This makes the grid the width of the page
+    // This makes the grid 'multi-select', adds the checkboxes for selecting to the side
+    myGrid.setSelectionMode(SelectionMode.MULTI);
+    MultiSelect <Bus> selected = myGrid.asMultiSelect();
 
-// Add the grid to the list
-layout.addComponent(myGrid);
+    // Add the grid to the list
+    layout.addComponent(myGrid);
 //********************End Grid/ Database***********************************
 
 //********************Start other Components******************** */
-Label heading = new Label();
+    Label heading = new Label();
         heading.setContentMode(ContentMode.HTML); //HTML
         heading.setValue("<H1>Fun Bus Bookings</H1>" +"<p/>"+
         "<h3>Please enter the details below and click Book</h3>");
         
         
-        Label result= new Label("Your party is not booked yet", ContentMode.HTML);
+    Label result= new Label("Your party is not booked yet", ContentMode.HTML);
         
 
-        Label studentNo = new Label("B00766612");
+    Label studentNo = new Label("B00766612");
 
-        final TextField groupName= new TextField("Name of Group");
+    TextField groupName= new TextField("Name of Group");
         groupName.setMaxLength(25);
         groupName.setPlaceholder("");
 
-        Slider people = new Slider("How many people are coming to this party", 0, 500);
+    Slider people = new Slider("How many people are coming to this party", 0, 500);
         people.setValue(0.0); //starting value
         people.setWidth(people.getMax()+"px");
-   
         
-        ComboBox <String> accessible = new ComboBox<String>("Accessibility?");
+    ComboBox <String> accessible = new ComboBox<String>("Accessibility?");
         accessible.setItems("Yes", "No");
 
-        Button button = new Button("BOOK");
+    Button button = new Button("BOOK");
         button.addClickListener(e -> {
         
             Boolean bookable= true;  //is it bookable?
             Boolean isAccessible = selected.getValue().stream().map(Bus::isAccessible).anyMatch(Predicate.isEqual(true));
             int arePeople = selected.getValue().stream().mapToInt(Bus::getPeople).sum();
-             //check grid selection
+             
+            //check grid selection
              if(myGrid.getSelectedItems().size() == 0)
              {
                  bookable=false; //not bookable
                  result.setValue("<strong>Please select at least one Bus!</strong>");
                  return;
              }
-         //check party name entried
+         
+            //check party name entried
              if(groupName.getValue().length() == 0)
              {
                  bookable=false; //not bookable
                  result.setValue("<strong>Please enter party name.</strong>");
                  return;
              }
-         //check children selection
-         if(!accessible.getSelectedItem().isPresent())
+            //check children selection
+            if(!accessible.getSelectedItem().isPresent())
              {
                  bookable=false; //not bookable
                  result.setValue("<strong>Please confirm if children attending your party</strong>");
                  return;
              }
  
-         //check children/alcohol
-             //if ((isAlcohol.equals(yesAlco))&&(children.getValue().equals("Yes")))
+            //check children/alcohol
              if ((accessible.getValue() == "Yes") && (isAccessible.equals(true)))
              { 
                  bookable=false; //not bookable
-                result.setValue("<strong> You cannot select any Buses that are not accessible.</strong>");
+                 result.setValue("<strong> You cannot select any Buses that are not accessible.</strong>");
                  return;
              }
         
              
-         //check capacity of room
-           if (arePeople<people.getValue())
+            //check capacity of room
+            if (arePeople<people.getValue())
              { 
                   bookable=false; //not bookable
                   result.setValue("<strong>You have selected buses with a max capacity of "+ arePeople+  " which is not enough to hold " +people.getValue().intValue() +"</strong>");
-                 return;
+                  return;
              }
              
 
-         //all ok
-         if (bookable==true)
-         {
-             result.setValue("<h3>Success! The party is booked now</h3>");
-             return;
-         }
+            //all ok
+            if (bookable==true)
+            {
+                result.setValue("<h3>Success! The party is booked now</h3>");
+                return;
+            }
 
-
-         layout.addComponent(new Label("Thanks " + groupName.getValue() 
-                 + ", it works!"));
-     });
+        }); //end of Button Click
      //************ End of other Components and calculations******* */
+     
      //------------ Add Components to Layout ------------------------/
-
-
      horizontalLayout.addComponents(groupName, people, accessible);
      layout.addComponents(heading, horizontalLayout, button, result, myGrid, studentNo);
      setContent(layout);
@@ -184,7 +181,6 @@ catch (Exception e)
 	layout.addComponent(new Label(e.getMessage()));
 } //catch
         
-    
     
     }
 
